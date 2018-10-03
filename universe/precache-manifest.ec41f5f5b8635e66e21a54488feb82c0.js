11411eb96b81d367d9049594d8097561003c518d
alexiej <arek.klemenko@gmail.com>
2018-10-03 16:44:17 +0200

    publish

---
+++ universe/precache-manifest.ec41f5f5b8635e66e21a54488feb82c0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "350d697803693ac007e1067dcf6b6102",
+    "url": "icons/data-science.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "257d9988fe5f77099b4c7f991b976a29",
+    "url": "icons/ml.svg"
+  },
+  {
+    "revision": "b3220f104a483dc76cc3",
+    "url": "js/app.fc549516.js"
+  },
+  {
+    "revision": "eb18fe06be5b68c7e254",
+    "url": "js/chunk-vendors.0a7ffc96.js"
+  },
+  {
+    "revision": "821915767a09a943bcdcdf486781c398",
+    "url": "md/r.md"
+  },
+  {
+    "revision": "c0e5c1ac53a09a2ba849ae29b9a57775",
+    "url": "md/python.md"
+  },
+  {
+    "revision": "e0e4fcdc29c566ce668065aa2f884eb2",
+    "url": "md/data-science.md"
+  },
+  {
+    "revision": "eacdc3f1279a93dbf11bd59e73927691",
+    "url": "json/universe.json"
+  },
+  {
+    "revision": "23354afce5a855f8291a",
+    "url": "js/about.d4a2497d.js"
+  },
+  {
+    "revision": "3a8a2dfb69d698f6ed384c540e570566",
+    "url": "index.html"
+  },
+  {
+    "revision": "20f8acf1f53241f2a7504b9aca00b754",
+    "url": "icons/r.svg"
+  },
+  {
+    "revision": "e98346ee9b9f6d54e9e62a1e5b4d233c",
+    "url": "icons/python.svg"
+  },
+  {
+    "revision": "da23a3ecf1f328200d3458ccb920b2a4",
+    "url": "icons/math.svg"
+  },
+  {
+    "revision": "25fceda2c35d50dcc6cbc4f8a4b35288",
+    "url": "icons/computer.svg"
+  },
+  {
+    "revision": "6f0a76321d30f3c8120915e57f7bd77e",
+    "url": "fonts/element-icons.6f0a7632.ttf"
+  },
+  {
+    "revision": "2fad952a20fbbcfd1bf2ebb210dccf7a",
+    "url": "fonts/element-icons.2fad952a.woff"
+  },
+  {
+    "revision": "eb18fe06be5b68c7e254",
+    "url": "css/chunk-vendors.77f1aa10.css"
+  },
+  {
+    "revision": "b3220f104a483dc76cc3",
+    "url": "css/app.b37590f1.css"
+  }
+];
