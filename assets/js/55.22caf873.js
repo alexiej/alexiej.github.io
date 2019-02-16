54bcbb236f90cf8c1bd89dbf36b512b84573ac99
alexiej <arek.klemenko@gmail.com>
2019-02-16 19:51:19 +0100

    # Deep Network
    * Correct information about learning rate and
    how to pick up learning rate()
    * Add information about export and load_learner()
    * Add information about fit1cycle
    * Add information about normalize()
    * Add information about Google Drive on Google Colab
    * Add Download Google Images
    * Add example for SGD from fast.ai
    
    # Add article for Creating production server

---
+++ assets/js/55.22caf873.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[55],{1021:function(t,n,e){"use strict";e.r(n);e(975);var s=e(1),a=Object(s.a)({},function(){var t=this.$createElement;return(this._self._c||t)("span",{staticClass:"red"},[this._t("default")],2)},[],!1,null,"9e33b382",null);n.default=a.exports},229:function(t,n,e){},975:function(t,n,e){"use strict";var s=e(229);e.n(s).a}}]);
